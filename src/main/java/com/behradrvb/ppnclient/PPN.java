9c262557d4ed17aadabdcf28235133bca3187c7c
Behrad Ravanbod <99behi@gmail.com>
2019-09-12 13:42:03 +0430

    SharedPref added.

--- src/main/java/com/behradrvb/ppnclient/PPN.java
+++ src/main/java/com/behradrvb/ppnclient/PPN.java
@@ -1,6 +1,7 @@
 package com.behradrvb.ppnclient;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 public class PPN {
     private Context context;
@@ -12,10 +13,18 @@ public class PPN {
         this.context = context;
         this.host = host;
         this.port = port;
-        this.interiorBroadcast = interiorBroadcast;
+        this.interiorBroadcast = interiorBroadcast; // "true" if you want to use interior broadcast receiver: which run SocketService with data that was saved into sharedPref by app
     }
 
     public void init() {
+        setPreferences();
+    }
 
+    private void setPreferences() { //This function saves data in SharedPreferences
+        SharedPreferences.Editor editor = context.getSharedPreferences("PPN", Context.MODE_PRIVATE).edit();
+        editor.putString("host", host);
+        editor.putInt("port", port);
+        editor.putBoolean("interiorBroadcast", interiorBroadcast);
+        editor.apply();
     }
 }
